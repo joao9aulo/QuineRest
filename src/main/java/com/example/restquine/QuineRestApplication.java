73e30f85f6b3433ef710a0815b77e6fd15f5f488
João Paulo <joao9aulo@gmail.com>
2023-12-13 21:29:16 -0300

    desenvolvimento

--- src/main/java/com/example/restquine/QuineRestApplication.java
+++ src/main/java/com/example/restquine/QuineRestApplication.java
@@ -16,16 +16,14 @@ import java.nio.file.Files;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+@RestController
 @SpringBootApplication
 public class QuineRestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QuineRestApplication.class, args);
     }
-}
 
-@RestController
-class QuineController {
 
     @GetMapping("/jar")
     public ResponseEntity<byte[]> getJar() throws IOException {
@@ -33,13 +31,13 @@ class QuineController {
         char newLine = 10;
         String code = """
                 package com.example.restquine;
-
+                                
                 import org.springframework.boot.SpringApplication;
                 import org.springframework.boot.autoconfigure.SpringBootApplication;
                 import org.springframework.http.*;
                 import org.springframework.web.bind.annotation.GetMapping;
                 import org.springframework.web.bind.annotation.RestController;
-
+                                
                 import javax.tools.JavaCompiler;
                 import javax.tools.ToolProvider;
                 import java.io.ByteArrayOutputStream;
@@ -49,66 +47,56 @@ class QuineController {
                 import java.nio.file.Files;
                 import java.util.zip.ZipEntry;
                 import java.util.zip.ZipOutputStream;
-
+                                
+                @RestController
                 @SpringBootApplication
                 public class QuineRestApplication {
-
+                                
                     public static void main(String[] args) {
                         SpringApplication.run(QuineRestApplication.class, args);
                     }
-                }
-
-                @RestController
-                class QuineController {
-
+                                
+                                
                     @GetMapping("/jar")
                     public ResponseEntity<byte[]> getJar() throws IOException {
-                        // Prepare source code
-                        String source = %s;
-
-                        // Compile source code
+                        String textBlockQuotes = new String(new char[]{'"', '"', '"'});
+                        char newLine = 10;
+                        String code = %s;
                         JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-                        byte[] classBytes = compileSource(source, compiler);
-
-                        // Create JAR in memory
+                        String formatedCode = code.formatted(textBlockQuotes + newLine + code + textBlockQuotes);
+                        System.out.print(formatedCode);
+                        byte[] classBytes = compileSource(formatedCode, compiler);
+                                
                         ByteArrayOutputStream jarOutputStream = createJar(classBytes);
-
-                        // Create response
+                                
                         HttpHeaders headers = new HttpHeaders();
                         headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-                        ContentDisposition  contentDisposition = ContentDisposition.builder("attachment").filename("test.jar").build();
+                        ContentDisposition contentDisposition = ContentDisposition.builder("attachment").filename("test.jar").build();
                         headers.setContentDisposition(contentDisposition);
-
+                                
                         return new ResponseEntity<>(jarOutputStream.toByteArray(), headers, HttpStatus.OK);
                     }
-
+                                
                     private byte[] compileSource(String source, JavaCompiler compiler) throws IOException {
                         File tempDir = Files.createTempDirectory("java").toFile();
                         File sourceFile = new File(tempDir, "test/QuineRestApplication.java");
                         sourceFile.getParentFile().mkdirs();
                         Files.write(sourceFile.toPath(), source.getBytes(StandardCharsets.UTF_8));
-
-                        // Compile source file
+                                
                         compiler.run(null, null, null, "-d", tempDir.getAbsolutePath(), sourceFile.getPath());
-
-                        // Read compiled class file
-                        File classFile = new File(tempDir, "test/Test.class");
+                                
+                        File classFile = new File(tempDir, "test/QuineRestApplication.class");
                         return Files.readAllBytes(classFile.toPath());
                     }
-
+                                
                     private ByteArrayOutputStream createJar(byte[] classBytes) throws IOException {
                         ByteArrayOutputStream jarOutputStream = new ByteArrayOutputStream();
                         ZipOutputStream zipOutputStream = new ZipOutputStream(jarOutputStream);
-
-                        // Add class file to JAR
-                        ZipEntry classEntry = new ZipEntry("test/Test.class");
+                        ZipEntry classEntry = new ZipEntry("test/QuineRestApplication.class");
                         zipOutputStream.putNextEntry(classEntry);
                         zipOutputStream.write(classBytes);
                         zipOutputStream.close();
-
-                        // Close ZIP stream
                         zipOutputStream.close();
-
                         return jarOutputStream;
                     }
                 }
@@ -129,13 +117,13 @@ class QuineController {
     }
 
     private byte[] compileSource(String source, JavaCompiler compiler) throws IOException {
-        File tempDir = Files.createTempDirectory("java").toFile();
+        File tempDir = Files.createTempDirectory("test").toFile();
         File sourceFile = new File(tempDir, "test/QuineRestApplication.java");
         sourceFile.getParentFile().mkdirs();
         Files.write(sourceFile.toPath(), source.getBytes(StandardCharsets.UTF_8));
 
         compiler.run(null, null, null, "-d", tempDir.getAbsolutePath(), sourceFile.getPath());
-
+        System.out.print(tempDir.listFiles());
         File classFile = new File(tempDir, "test/QuineRestApplication.class");
         return Files.readAllBytes(classFile.toPath());
     }
@@ -143,7 +131,7 @@ class QuineController {
     private ByteArrayOutputStream createJar(byte[] classBytes) throws IOException {
         ByteArrayOutputStream jarOutputStream = new ByteArrayOutputStream();
         ZipOutputStream zipOutputStream = new ZipOutputStream(jarOutputStream);
-        ZipEntry classEntry = new ZipEntry("test/Test.class");
+        ZipEntry classEntry = new ZipEntry("test/QuineRestApplication.class");
         zipOutputStream.putNextEntry(classEntry);
         zipOutputStream.write(classBytes);
         zipOutputStream.close();
